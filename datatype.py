9700d2577af3d57b36d0abcf12e3de9b2923fce1
lalit <lalitsingh958954>
2026-06-21 21:57:52 +0530

    first python prosgram

--- datatype.py
+++ datatype.py
@@ -1,11 +1,82 @@
-## data type
-# string , floating ,int ,boolean
+# ## data type
+# # string , floating ,int ,boolean
+#
+# ## int
+# # print("data type")
+# var1 =1
+# # print(type(var1))
+# var2 =3
+# print(var1 + var2)
+# var3="""lalit's is name "second" """
+# print(var3)
+# print(type(var3))
+#
+# var3 ="HEllo"
+# print(var3.isupper())
+#
+#
+# print(f" sum of {var1} and {var2} is:{var1+var2}")
+#
+# ### type conversion
+#
+#
+# age ="35"
+#
+# print(type(age))
+#
+# age_convert = (int(age))
+#
+# print(type(age_convert))
+#
+#
+# new_age=  "5"
+# total_age = int(age) + int(new_age)
+# print("age total is \n",total_age)
+#
+#
+# print(type(age))
 
-## int
+print("hello i am the best")
 
-# print("data type")
-var1 =1
-# print(type(var1))
-var2 =3
-print(var1 + var2)
+
+## list exercise
+#
+# servers = ["web1", "web2", "app1"]
+#
+# print(servers)
+# servers.append("web3")
+# print(servers)
+# servers.remove("app1")
+# print(servers)
+#
+# print(len(servers))
+
+
+#### dictonary operations
+
+# ec2_instance = {
+#     "instance_id": "i-12345",
+#     "instance_type": "t3.medium",
+#     "state": "running"
+# }
+#
+# print(ec2_instance["state"])
+#
+# ec2_instance["region"] = "ap-south-1"
+#
+# ec2_instance["state"] = "stopped"
+#
+# print(ec2_instance)
+
+#####################
+
+#Type Casting
+
+
+
+
+
+
+
+
 
