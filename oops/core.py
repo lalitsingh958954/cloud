9c67722de18e5c9fed9d494a4fb87b02f2efc033
lalit <lalitsingh958954>
2026-06-29 02:12:23 +0530

    first python prosgram

---
+++ oops/core.py
@@ -0,0 +1,6 @@
+class Calculator:
+    def sum(self,a,b):
+        return a+b
+    def sub(self,a,b):
+        return a-b
+    
---
+++ oops/demo.py
@@ -0,0 +1,9 @@
+# from core file import calculator
+from core import Calculator
+
+obj = Calculator()
+
+response =obj.sum(7,3)
+
+print(response)
+
